9b1c7d8813fda68e9d91307a22caef54c826709f
meirza23 <114686501+meirza23@users.noreply.github.com>
2022-10-27 14:28:35 +0300

    Add files via upload

---
+++ c school homeworks/A DAN Z YE TÜM HARFLER.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	
+    char harf;
+    
+    for (harf = 'A'; harf <= 'Z'; harf++){
+        
+		printf("%c ", harf);
+    }
+    
+    getch();
+    
+    return 0;
+}
